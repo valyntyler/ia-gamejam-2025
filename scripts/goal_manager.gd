fac8e685c30d09dd965957c01178c7a191adb97d
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-12-14 13:09:27 +0100

    feat: lerping to target

--- scripts/goal_manager.gd
+++ scripts/goal_manager.gd
@@ -2,14 +2,28 @@ extends Node2D
 
 @onready var vignette: ColorRect = %Vignette
 
-var perc: float = 1.0:
+var target: float
+var perc: float:
 	set(value):
+		perc = value
 		vignette.material.set("shader_parameter/outer_radius", value)
 
 
+func lerp_vignette(weight: float):
+	perc = lerp(perc, target, weight)
+	print(perc)
+
+
 func _ready() -> void:
-	perc = 0.1
+	perc = 0.0
 
 
-func _process(_delta: float) -> void:
-	perc = abs(sin(Time.get_ticks_msec() / 1000.0))
+func _process(delta: float) -> void:
+	lerp_vignette(delta)
+	if (
+		Input.is_action_pressed("move_left")
+		|| Input.is_action_pressed("move_right")
+		|| Input.is_action_pressed("move_up")
+		|| Input.is_action_pressed("move_down")
+	):
+		target = 1
